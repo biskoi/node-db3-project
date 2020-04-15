5249d96c80b56b63a9c8d57e6ae75f923940e0e5
biskoi <biskoi@daydream.cafe>
2020-04-15 14:13:45 -0600

    Finished writing queries.sql

--- queries.sql
+++ queries.sql
@@ -2,8 +2,29 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
--- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT * 
+FROM Product AS p
+JOIN Category AS c ON p.CategoryId = c.id
+
+-- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records. 2012-8-9
+
+SELECT o.id, o.OrderDate, s.CompanyName
+FROM 'order' AS o
+JOIN Shipper AS s ON s.Id = o.ShipVia
+WHERE o.OrderDate < date('2012-08-09');
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT o.id, od.ProductId, p.ProductName, od.Quantity
+FROM 'order' AS o
+JOIN OrderDetail AS od ON od.OrderId = o.id
+JOIN Product AS p ON p.Id = od.ProductId
+WHERE o.id = 10251
+ORDER BY p.productName
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.id AS 'Order ID', c.CompanyName, e.LastName
+FROM 'Order' AS o
+JOIN Customer AS c ON o.CustomerId = c.id
+JOIN Employee AS e ON e.Id = o.EmployeeId
